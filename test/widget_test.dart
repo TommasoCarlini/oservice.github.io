413f4dbaefd3faad77c17fcd35945602b3746d54
t.carlini <to.carlini@gmail.com>
2025-03-18 13:09:07 +0100

    add images to assets

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:o_service_db/main.dart';
+import 'package:oservice/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
